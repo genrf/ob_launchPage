6d44da8da540b33231a9098080d0460e4ed56428
danieldisu <danieldiazsuarez@gmail.com>
2013-06-17 19:13:45 +0200

    Cambios en el active de los links

--- script/script.js
+++ script/script.js
@@ -1,3 +1,10 @@
 (function($){
 	$('.carousel').carousel();
+
+  window.onhashchange = function(){
+    $('.active').removeClass('active');
+    $('a[href="' + window.location.hash + '"]').parent().addClass('active');
+     
+  }
+
 })(jQuery);
